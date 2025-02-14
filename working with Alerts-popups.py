30a3b57cd2835226e7021ce128def3c91e287eb8
Asad Ur Rehman <mr.asad.dev@gmail.com>
2025-02-15 00:12:05 +0500

    Alerts and Popups

--- working with Alerts-popups.py
+++ working with Alerts-popups.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe1499b069d28e56dd13f7214088fa503dc93f31523f07a60d9cefa01b4a0f08
-size 1906
+oid sha256:39163b0472daca4205753aa37ce1512992f1834cf9f00066a1ff323664e570fc
+size 1863
