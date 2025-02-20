877508d7313bfe976c5d1956d81f47d81501be2e
Asad Ur Rehman <mr.asad.dev@gmail.com>
2025-02-20 21:10:03 +0500

    Working with Web Tables - Static Table

--- working with Web Tables-Static.py
+++ working with Web Tables-Static.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec886a252206ddb6d5d6a341357f5c059df9c70fb0ceb392870982289b747150
-size 2117
+oid sha256:dc865ac40e500321cc46d235d1e4c59cce18287c8bf451f40fc3f04b4c08b756
+size 2121
