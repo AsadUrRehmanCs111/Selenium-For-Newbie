4d91d86df4fac73da6451a9904d1a0c801fe9414
Asad Ur Rehman <mr.asad.dev@gmail.com>
2025-02-26 23:25:12 +0500

    Working on Web Tables - Dynamic Tables

--- working with Web Tables-Dynamic.py
+++ working with Web Tables-Dynamic.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:82979e7346474a085090ae8bfbca9176a8068f4fd34d2cb02ed486aa32a987bb
-size 1480
+oid sha256:2608d1f096f4423a731321f36cbbb6e4b1d9d6914a2edd1f2194d525e076d19e
+size 1416
